a0633720dc54db08fd503e981fa7034bdba6c04c
admin <bjl0615@gmail.com>
2023-08-10 01:55:52 +0900

    Repository Search part_1

--- app/src/main/java/com/example/github_repository_query/RepoActivity.kt
+++ app/src/main/java/com/example/github_repository_query/RepoActivity.kt
@@ -1,17 +1,58 @@
 package com.example.github_repository_query
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.github_repository_query.adapter.RepoAdapter
 import com.example.github_repository_query.databinding.ActivityRepoBinding
+import com.example.github_repository_query.model.Repo
+import com.example.github_repository_query.network.GithubService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RepoActivity : AppCompatActivity() {
     private lateinit var binding : ActivityRepoBinding
+    private lateinit var repoAdapter : RepoAdapter
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRepoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val username = intent.getStringExtra("username") ?: return
 
+        binding.usernameTextView.text = username
+
+        repoAdapter = RepoAdapter()
+
+        binding.repoRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@RepoActivity)
+            adapter = repoAdapter
+        }
+
+        listRepo(username)
+    }
+
+    private fun listRepo(username : String) {
+        val githubService = retrofit.create(GithubService::class.java)
+        githubService.listRepos(username).enqueue(object: Callback<List<Repo>> {
+            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+               Log.e("MainActivity" , "List Repo : ${response.body().toString()}")
+
+                repoAdapter.submitList(response.body())
+            }
+
+            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+
+            }
+        })
     }
 }
